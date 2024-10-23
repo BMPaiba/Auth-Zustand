b7101f45f655b9eba401d7f9fe3f5748c050d1f8
Brian Paiba <brianmanuelpaiba@outlook.es>
2024-10-23 20:48:51 -0300

    primer store

---
+++ src/stores/bears/bears.store.ts
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+
+interface BearState {
+  blackBears: number;
+  polarBears: number;
+  pandaBears: number;
+  increaseBlackBears: (by: number) => void;
+  increasePolarBears: (by: number) => void;
+  increasePandaBears: (by: number) => void;
+}
+
+export const useBearStore = create<BearState>()((set) => ({
+  blackBears: 10,
+  pandaBears: 10,
+  polarBears: 10,
+  increaseBlackBears: (by) => set((state) => ({ blackBears: state.blackBears + by })),
+  increasePolarBears: (by) => set((state) => ({ polarBears: state.polarBears + by })),
+  increasePandaBears: (by) => set((state) => ({ pandaBears: state.pandaBears + by })),
+}));
